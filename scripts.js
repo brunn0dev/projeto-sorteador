c28b08a5da9d38d1ee52c5a3835c7ae0eeadfc27
brunn0dev <brunnodev@hotmail.com>
2024-04-20 21:37:45 -0300

    novo-alert

--- scripts.js
+++ scripts.js
@@ -2,7 +2,13 @@ function gerador() {
     const min = Math.ceil(document.querySelector(".one").value)
     const max = Math.floor(document.querySelector(".two").value)
 
+    if (min>= max){
+    alert("O Valor minimo tem que ser MENOR do que o que o valor MAXÌMO.")
+    }
+    else{   
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
     alert(result)
     }
+
+   } 
